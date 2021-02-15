2e58186149acf6d067ba8495c377655cbf60abe1
mahdi <mohammadmahdiabdollahpoor@gmail.com>
2021-02-15 14:55:39 +0330

    code added

---
+++ JBDA_model_stealing_and_obfuscated_gradients/model.py
@@ -0,0 +1,26 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+  def __init__(self):
+    super(Net, self).__init__()
+    self.conv1 = nn.Conv2d(1, 16, 3, 1)
+    self.conv2 = nn.Conv2d(16, 32, 3, 1)
+    self.dropout1 = nn.Dropout2d(0.25)
+    self.dropout2 = nn.Dropout2d(0.5)
+    self.fc1 = nn.Linear(4608, 64)
+    self.fc2 = nn.Linear(64, 10)
+
+  def forward(self, x):
+    x = self.conv1(x)
+    x = F.relu(x)
+    x = self.conv2(x)
+    x = F.relu(x)
+    x = F.max_pool2d(x, 2)
+    x = self.dropout1(x)
+    x = torch.flatten(x, 1)
+    x = self.fc1(x)
+    x = F.relu(x)
+    x = self.dropout2(x)
+    x = self.fc2(x)
+    return x
